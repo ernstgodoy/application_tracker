d38b24b461be532ff9f82c28b351c696b9ce83d4
ernesto <ernsto.godoy@gmail.com>
2020-09-07 22:41:30 -0700

    added sign in link

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,9 +2,24 @@ import React from "react"
 import PropTypes from "prop-types"
 class App extends React.Component {
   render () {
+    const { 
+      logged_in,
+      sign_in_route,
+      sign_out_route
+    } = this.props
     return (
       <React.Fragment>
         <h1>React is Working</h1>
+        { logged_in &&
+          <div>
+            <a href={sign_out_route}>Sign Out</a>
+          </div>
+        }
+        { !logged_in &&
+          <div>
+            <a href={sign_in_route}>Sign In</a>
+          </div>
+        }
       </React.Fragment>
     );
   }
