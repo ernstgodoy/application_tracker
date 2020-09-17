61c32dd8d8717500005bf02a0877ca4b1d63d44c
ernesto <ernsto.godoy@gmail.com>
2020-09-17 09:28:18 -0700

    minor change to api call

--- app/javascript/components/Shared/ApiCalls.js
+++ app/javascript/components/Shared/ApiCalls.js
@@ -1,6 +1,6 @@
 const getRequest = (hook) => {
   return(
-    fetch(`http://localhost:3000/jobs`)
+    fetch(`/jobs`)
     .then((response) => response.json())
     .then((data) => hook(data))
   )
