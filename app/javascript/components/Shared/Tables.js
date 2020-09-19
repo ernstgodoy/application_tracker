a14f331141c65cbc2b69941e2a2da7806e5c746f
ernesto <ernsto.godoy@gmail.com>
2020-09-19 12:52:09 -0700

    populated edit form

--- app/javascript/components/Shared/Tables.js
+++ app/javascript/components/Shared/Tables.js
@@ -1,12 +1,19 @@
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Table, Container } from "react-bootstrap"
+import { Table, Container, Button } from "react-bootstrap"
 
 const Tables = (props) => {
   const data = props.data.map(d => d)
+
+  const handleDelete = () => {
+    console.log("delete button")
+  }
+
   return (
     <React.Fragment>
       <Container>
-        <Table striped bordered hover>
+        <Table hover responsive>
           <thead>
             <tr>
               <th>Company Name</th>
@@ -14,11 +21,13 @@ const Tables = (props) => {
               <th>Application Status</th>
               <th>Date Applied</th>
               <th>Last Follow Up</th>
+              <th>Edit/Delete</th>
             </tr>
           </thead>
           <tbody>
             { data && data.map((d, i) => {
               const {
+                id,
                 company,
                 title,
                 status,
@@ -26,12 +35,16 @@ const Tables = (props) => {
                 last_follow_up
               } = d
               return(
-                <tr key={i} >
-                  <td>{ company } </td>
+                <tr key={i}>
+                  <td>{ company }</td>
                   <td>{ title }</td>
                   <td>{ status }</td>
                   <td>{ date_applied }</td>
                   <td>{ last_follow_up }</td>
+                  <td>
+                    <Button href={`/edit/${id}`} variant="success" size="sm"><FontAwesomeIcon icon={faEdit} size="xs" /></Button>
+                    <Button onClick={ handleDelete } size="sm"><FontAwesomeIcon icon={faTrash} size="xs" /></Button>
+                  </td>
                 </tr>
               )
             })}
