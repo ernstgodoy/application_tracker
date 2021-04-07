f3dabe5fd1a30b1db11e59447765b0a71af51e3a
ernstgodoy <ernst.godoy@gmail.com>
2021-04-06 17:40:33 -0700

    added scope

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,6 @@ class Job < ApplicationRecord
   belongs_to :user
   validates_presence_of :user_id, :company, :title, :status, :date_applied, :last_follow_up
 
+  scope :filter_by_role, -> (role) { where title: role }
+  scope :filter_by_role, -> (status) { where status: status }
 end
