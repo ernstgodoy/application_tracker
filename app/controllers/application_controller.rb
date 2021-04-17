52e86d39784332c0afc3f758005cf248e72fe62e
ernstgodoy <ernst.godoy@gmail.com>
2021-04-16 17:20:42 -0700

    devise to accept new props

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
 
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parameters
+			devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :first_name, :last_name, :job_title, :password_confirmation])
+	end
+
   private
 
 	def after_sign_in_path_for(resource)
